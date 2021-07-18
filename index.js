e736fa2c4cdbff4179e5d0b5e6db5fa198c702ef
chimimode <chimimode@gmail.com>
2021-07-18 18:16:05 +0900

    init project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/questions', (req, res) => {
+  res.sendFile(path.join(__dirname, '/data', 'question.json'));
+});
+
+app.get('/question/:questionId', (req, res) => {
+  const qeustionId = parseInt(req.params.questionId);
+  
+  fs.readFile(path.join(__dirname, '/data', 'question.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    data = JSON.parse(data);
+
+    res.json(data.questions[qeustionId]);
+  });
+
+});
+
+app.listen(3000);
